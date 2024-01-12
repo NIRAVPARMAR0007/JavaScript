f4546f642b0f1e171ddc62e90a8c2e4afc68df75
NIRAVPARMAR0007 <90770120+NIRAVPARMAR0007@users.noreply.github.com>
2024-01-12 08:26:33 +0000

    my first test

---
+++ MyProgram/test.js
@@ -0,0 +1,13 @@
+console.log("Hi!, I am JavaScript")
+
+let name = "Nirav"
+var email = "np123@gmail.com" // can't using out of block
+const id = 14 // can't change
+
+{
+    let name = "Jignesh"
+}
+
+email = "jp@gmail.com"
+
+console.table([name, email, id])
